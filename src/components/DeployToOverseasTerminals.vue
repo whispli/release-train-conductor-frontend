dedde5c3c7c8cb97c74968dd3fc5717d55a917e9
Ruben Funai <ruben.funai@gmail.com>
2019-09-08 09:48:07 +1000

    Added deploy to overseas terminals.

---
+++ src/components/DeployToOverseasTerminals.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-container class="fill-height" fluid>
+        <v-row>
+            <v-col cols="12">
+                <div class="text-center">
+                    <v-btn>Deploy to Overseas Terminals</v-btn>
+                </div>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({}),
+  };
+</script>
