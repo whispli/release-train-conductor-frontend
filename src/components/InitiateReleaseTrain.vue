5fc18e121742b4cd66809590169574f30e1a44dc
Ruben Funai <ruben.funai@gmail.com>
2019-09-08 00:12:18 +1000

    Added Initiate Release Train component.

---
+++ src/components/InitiateReleaseTrain.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-container class="fill-height" fluid>
+        <v-row>
+            <v-col cols="12">
+                <div class="text-center">
+                    <v-btn>Initiate Release Train</v-btn>
+                </div>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({}),
+  };
+</script>
