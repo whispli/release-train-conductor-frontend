5f401b53e6bac9b27c287d554879ff2e4a707e98
Ruben Funai <ruben.funai@gmail.com>
2019-09-11 23:11:46 +1000

    Implemented deploy release plane logic.

--- src/App.vue
+++ src/App.vue
@@ -49,11 +49,20 @@
           </v-col>
         </v-row>
       </v-container>
-      <v-container class="fill-height" fluid v-if="show.DeployToOverseasTerminals">
+      <v-container class="fill-height" fluid v-if="show.PrepareReleasePlane">
         <v-row>
           <v-col cols="12">
             <div class="text-center">
-              <v-btn>Deploy to Overseas Terminals</v-btn>
+              <v-btn @click="prepareReleasePlane()" x-large color="success" >Prepare Release Plane</v-btn>
+            </div>
+          </v-col>
+        </v-row>
+      </v-container>
+      <v-container class="fill-height" fluid v-if="show.DeployReleasePlane">
+        <v-row>
+          <v-col cols="12">
+            <div class="text-center">
+              <v-btn @click="deployReleasePlane()" x-large color="info">Deploy Release Plane</v-btn>
             </div>
           </v-col>
         </v-row>
@@ -73,6 +82,8 @@ export default {
     show: {
       PrepareReleaseTrain: true,
       DeployReleaseTrain: false,
+      PrepareReleasePlane: false,
+      DeployReleasePlane: false,
       DeployToOverseasTerminals: false,
       isLoading: false,
       deployReleaseTrainConfirmationDialog: false
@@ -107,6 +118,15 @@ export default {
     deployReleaseTrainPullRequest: async (apiBaseUri, repoSlug, pullRequestId) => {
       return axios.patch(apiBaseUri + `/repositories/${repoSlug}/release-train-pull-requests/${pullRequestId}`)
     },
+    getReleasePlanePullRequests: async (apiBaseUri, repoSlug) => {
+      return axios.get(apiBaseUri + `/repositories/${repoSlug}/release-plane-pull-requests`)
+    },
+    createReleasePlanePullRequest: async (apiBaseUri, repoSlug) => {
+      return axios.post(apiBaseUri + `/repositories/${repoSlug}/release-plane-pull-requests`)
+    },
+    deployReleasePlanePullRequest: async (apiBaseUri, repoSlug, pullRequestId) => {
+      return axios.patch(apiBaseUri + `/repositories/${repoSlug}/release-plane-pull-requests/${pullRequestId}`)
+    },
     checkForPullRequests: async function () {
       this.show.isLoading = true
       for (let repository of this.repositories) {
@@ -123,18 +143,47 @@ export default {
         this.showDeployReleaseTrain()
       } else {
         this.showPrepareReleaseTrain()
+        await this.checkForReleasePlanePullRequests()
+      }
+      this.show.isLoading = false
+    },
+    checkForReleasePlanePullRequests: async function () {
+      this.show.isLoading = true
+      for (let repository of this.repositories) {
+        const response = await this.getReleasePlanePullRequests(this.apiBaseUri, repository.slug)
+
+        if (response.data.data) {
+          this.repositoryPullRequests[repository.slug] = response.data.data
+        } else {
+          delete this.repositoryPullRequests[repository.slug]
+        }
+      }
+
+      if (Object.keys(this.repositoryPullRequests).length > 0) {
+        this.showDeployReleasePlane()
+      } else {
+        this.showPrepareReleaseTrain()
       }
       this.show.isLoading = false
     },
     prepareReleaseTrain: async function () {
+      let createdPullRequest = false
+
       this.show.isLoading = true
       for (let repository of this.repositories) {
         try {
           await this.createReleaseTrainPullRequest(this.apiBaseUri, repository.slug)
+          createdPullRequest = true
         } catch (err) {
           // Just ignore if the PR could not be created.
           // TODO: Read the payload and check that it failed because there were no changes to be pulled
         }
+      }
+
+      if (!createdPullRequest) {
+        this.showPrepareReleasePlane()
+        this.show.isLoading = false
+        return;
       }
 
       await this.checkForPullRequests()
@@ -155,6 +204,35 @@ export default {
         this.show.isLoading = false
       })
     },
+    prepareReleasePlane: async function () {
+      this.show.isLoading = true
+      for (let repository of this.repositories) {
+        try {
+          await this.createReleasePlanePullRequest(this.apiBaseUri, repository.slug)
+        } catch (err) {
+          // Just ignore if the PR could not be created.
+          // TODO: Read the payload and check that it failed because there were no changes to be pulled
+        }
+      }
+
+      await this.checkForReleasePlanePullRequests()
+      this.show.isLoading = false
+    },
+    deployReleasePlane: function () {
+      this.show.isLoading = true
+      const promises = []
+
+      for (let repoSlug of Object.keys(this.repositoryPullRequests)) {
+        for (let i=0; i < this.repositoryPullRequests[repoSlug].length; i++) {
+          promises.push(this.deployReleasePlanePullRequest(this.apiBaseUri, repoSlug, this.repositoryPullRequests[repoSlug][i].id))
+        }
+      }
+
+      Promise.all(promises).then(() => {
+        this.showPrepareReleaseTrain()
+        this.show.isLoading = false
+      })
+    },
     hideAll: function () {
       for (let componentName of Object.keys(this.show)) {
         this.show[componentName] = false
@@ -167,7 +245,15 @@ export default {
     showDeployReleaseTrain: function () {
       this.hideAll()
       this.show.DeployReleaseTrain = true
-    }
+    },
+    showPrepareReleasePlane: function () {
+      this.hideAll()
+      this.show.PrepareReleasePlane = true
+    },
+    showDeployReleasePlane: function () {
+      this.hideAll()
+      this.show.DeployReleasePlane = true
+    },
   }
 }
 </script>
